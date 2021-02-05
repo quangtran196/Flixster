d3f9677c7e2d7a5124ba4daa8b0d66ceed77b53f
quangtran196 <tranducquang196@gmail.com>
2021-02-05 13:32:41 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Tran Duc Quang on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
